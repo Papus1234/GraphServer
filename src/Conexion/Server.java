5d5eb100fa8b07675d2d473342027b521e750f54
r0b78 <robert19972008@hotmail.com>
2016-11-12 11:10:28 -0600

    .

--- src/Conexion/Server.java
+++ src/Conexion/Server.java
@@ -5,10 +5,26 @@
  */
 package Conexion;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * @author Roberto
  */
 public class Server {
+    public int Port=4444;
+    public Server(){
+    
+    }
+    public void runServer()throws IOException{
+        ServerSocket serverSocket=new ServerSocket(Port);
+        System.out.println("Servidor Listo ..");
+        while(1){
+        Socket socket=serverSocket.accept();
+        new ServerThread(socket).start();
+        }
+    }
     
 }
--- src/Conexion/ServerThread.java
+++ src/Conexion/ServerThread.java
@@ -5,10 +5,35 @@
  */
 package Conexion;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 /**
  *
  * @author Roberto
  */
-public class ServerThread {
+public class ServerThread extends Thread{
+    Socket socket;
+    public ServerThread(Socket socket){
+        this.socket=socket;
+        
+        }
+    public void run(){
+        try{
+        BufferedReader bufferedReader=new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+        String message=bufferedReader.readLine();
+        while (message=bufferedReader.readLine()!=null){
+            System.out.println("Llego mensaje");
+            
+        }
+        socket.close();
+        
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
     
 }
